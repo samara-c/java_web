995bef3651806f5623fe7b2a8668b38e8f7a6910
Samara <41492804+samara-c@users.noreply.github.com>
2020-10-24 15:10:15 -0300

    Add files via upload

---
+++ restapp/src/main/java/restapp/Professor.java
@@ -0,0 +1,18 @@
+package restapp;
+
+public class Professor {
+	
+	private long id;
+	private String nome;
+	private int matricula;
+	private String area;
+	
+	public Professor () {
+		super();
+	}
+	public Professor (long id, String nome, int matricula, String area) {
+
+}
+}
+
+
---
+++ restapp/src/main/java/restapp/ProfessorRestController.java
@@ -0,0 +1,10 @@
+package restapp;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfessorRestController {
+	private List <Professor> professores; 
+}
